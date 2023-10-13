fec075e6d5f0b58286a5956b00be455b67813263
krushna desai <krushnad001@gmail.com>
2023-10-13 13:01:06 +0530

    first push

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETMVCCRUD1.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD1.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
